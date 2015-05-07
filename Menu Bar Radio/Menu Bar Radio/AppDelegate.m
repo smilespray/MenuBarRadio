13393ff487e337195c1f886f8be48fa33cd3e4c5
Snorre Milde <snorre@Snorres-MacBook-Pro-Retina-13.local>
2015-05-07 21:13:17 +0200

    Empty project

---
+++ Menu Bar Radio/Menu Bar Radio/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  Menu Bar Radio
+//
+//  Created by Snorre Milde on 07/05/15.
+//  Copyright (c) 2015 Snorre Milde. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ Menu Bar Radio/Menu Bar Radio/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Menu Bar Radio
+//
+//  Created by Snorre Milde on 07/05/15.
+//  Copyright (c) 2015 Snorre Milde. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
